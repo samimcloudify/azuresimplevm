8018833dfe25932774079dfd46dad89ee5ac67b7
samimcloudify <48019359+samimcloudify@users.noreply.github.com>
2023-12-31 09:16:13 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Specify the Azure provider and version
+provider "azurerm" {
+features {}
+  subscription_id = "e1731ade-101e-426e-a2b9-2a1bdbcaaa89"
+  client_id       = "39f80b8a-18eb-4702-9411-0d940386971b"
+  client_secret   = "ZKL8Q~NPNcAlLi6WF4MOX7tGV~Rt-fz7LypAPatQ"
+  tenant_id       = "82c6f5fb-ebc9-45d7-9e71-6e5a64fe3256"
+}
